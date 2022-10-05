a7ce4ba1f93a4c30f02280465071ec938cb6efcd
ManalAwadDev <manal.awad.dev@gmail.com>
2022-10-05 17:47:12 +0100

    Returning mapped object

--- Controllers/CountriesController.cs
+++ Controllers/CountriesController.cs
@@ -2,9 +2,7 @@
 using CountryDetails.ExternalResources;
 using CountryDetails.IntermediateModels;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,24 +15,45 @@ namespace CountryDetails.Controllers
     public class CountriesController : ControllerBase
     {
         ExternalApiClient _client;
-        
+        private static MapperConfigurationExpression _mapper;
+
         public CountriesController()
         {
             _client = new ExternalApiClient();
+            _mapper = new MapperConfigurationExpression();
         }
 
         // GET: api/Countries
         [HttpGet("{countryCode}")]
-        public async Task<IEnumerable<Country>> GetCountryByCide(string countryCode)
+        public async Task<ActionResult> GetCountryByCide(string countryCode)
         {
 
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<CountryAPIReturn, Country>());
-            var mapper = new Mapper(config);
+            var countriesMapping = new MapperConfiguration(cfg => cfg.CreateMap<CountryAPIReturn, Country>());
+
+            //_mapper.CreateMap<Message, ErrorMessage>();
+            //_mapper.CreateMap<CountryAPIReturn, ErrorMessage>()
+            //    .ForPath(dest => dest, x => x.MapFrom(src => src.message.First()));
+
+            var errorMapping = new MapperConfiguration(_mapper);
+
+            var errorMapper = new Mapper(errorMapping);
+
+            var countriesMapper = new Mapper(countriesMapping);
 
             var countriesResult = await _client.GetCountryDetails(countryCode);
-            var countries = mapper.Map<List<Country>>(countriesResult);
 
-            return countries;
+            if (countriesResult.Any(x => Equals(x.message, null))) // no error
+            {
+
+                var countries = countriesMapper.Map<List<Country>>(countriesResult);
+
+                return Ok(countries);
+            }
+
+            // To Do create mapper for error message
+            var error = countriesResult?.First().message;
+            
+            return Ok(error);
         }
     }
 }
---
+++ IntermediateModels/ErrorMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace CountryDetails.IntermediateModels
+{
+    public class ErrorMessage
+    {
+        public string id { get; set; }
+        public string key { get; set; }
+        public string value { get; set; }
+
+    }
+}
